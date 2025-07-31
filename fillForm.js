d0a15df455b2b0548aa6f03b7ecaccd3ee1a1be9
KameJymy <christophe.wang@edu.devinci.fr>
2025-07-31 23:48:27 +0200

    infos rentrées (date-début/fin)

--- fillForm.js
+++ fillForm.js
@@ -6,6 +6,7 @@ async function fillForm(session)
     await session.get("http://127.0.0.1:5500/leadfoot-exo1/Doc_offi/Exo24/page-demande.html");
 
     const input_texts = await session.findAllByXpath(selectors.input_text);
+    const input_date = await session.findAllByXpath(selectors.input_date);
     const data = {
         prenom : "Jean",
         nom:"Dujardin",
@@ -18,7 +19,22 @@ async function fillForm(session)
         {
             await i.clearValue();
             await i.type(data[id]);
-            console.log(id +" : "+data[id]);
+            console.log(id+" : "+data[id]);
+        }
+    }
+
+    const data1 = {
+        "date-debut" :"01/01/2001",
+        "date-fin" :"01/01/2002"
+    };
+    for (let i of input_date)
+    {
+        const id = await i.getAttribute("id");
+        if (data1[id])
+        {
+            await i.clearValue();
+            await i.type(data1[id]);
+            console.log(id+" : "+data1[id]);
         }
     }
 }
--- selectors.js
+++ selectors.js
@@ -1,3 +1,4 @@
 module.exports = {
     input_text : "//form//label/input[@type ='text' or @type='email']",
+    input_date : "//form//label/input[@type= 'date']"
 };
